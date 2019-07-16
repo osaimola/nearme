0af4849ac29d14f133dfcd46c6fa50bae5f5c73a
Osa <39202977+osaimola@users.noreply.github.com>
2019-07-16 08:55:07 +0100

    add search from util/here

--- src/App.js
+++ src/App.js
@@ -2,21 +2,40 @@ import React from 'react';
 import './App.css';
 import {SearchResults} from "./Components/SearchResults/SearchResults";
 import {SearchBar} from "./Components/SearchBar/SearchBar";
+import {here} from "./util/here";
 
 const poi = {name: "West Edmonton Mall",
+  id: "123hghjd",
   address: "8882 170 St NW<br/>Edmonton, AB T5T",
   hours: "Mon-Sat: 10:00 - 21:00<br/>Sun: 11:00 - 18:00",
   isOpen:true
 };
-const pois = [poi, poi, poi, poi, poi, poi];
+//const pois = [poi, poi, poi, poi, poi, poi];
 
-function App() {
-  return (
-    <div>
-      <SearchBar />
-      <SearchResults searchResults={pois}/>
-    </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mapImage: "",
+      poiList: []
+    };
+    this.search = this.search.bind(this);
+  }
+
+  search(term, location) {
+    here.getMapImage(location).then( image => {console.log(image)});
+    //here.search(term, location).then(pois => this.setState({poiList: pois}));
+    console.log(term + " " + location);
+    // get list of pois and get a small map image
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar onSearch={this.search}/>
+        <SearchResults searchResults={this.state.poiList}/>
+      </div>
   );
+  }
 }
 
 export default App;
