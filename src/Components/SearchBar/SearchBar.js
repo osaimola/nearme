e1474d94b31b0dbeaee4e0d745a0bfd12f3eeb6a
Osa <39202977+osaimola@users.noreply.github.com>
2019-07-16 08:54:11 +0100

    add handle search function

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,15 +2,38 @@ import React from 'react';
 import './SearchBar.css';
 
 export class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: "",
+      location: ""
+    };
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.props.onSearch(this.state.term, this.state.location);
+  }
+
+  handleTermChange(e) {
+    this.setState({term: e.target.value});
+  }
+
+  handleLocationChange(e) {
+    this.setState({location: e.target.value});
+  }
+
   render() {
     return (
       <div className="SearchBar">
         <div className="SearchBar-fields">
-          <input type="text" placeholder="What are you looking for?" className="field"/>
-          <input type="text" placeholder="Where?" className="field" style={{width:30+"%"}}/>
+          <input placeholder="What are you looking for?" className="field"  onChange={this.handleTermChange}/>
+          <input placeholder="Where?" className="field" style={{width:30+"%"}}  onChange={this.handleLocationChange}/>
         </div>
         <div className="SearchBar-submit">
-          <a href="">Find</a>
+          <a onClick={this.handleClick}>Find</a>
         </div>
       </div>
     );
