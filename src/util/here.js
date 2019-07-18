0fe974e6c07d1399a27497d7df0308ab0ef946ee
Osa <39202977+osaimola@users.noreply.github.com>
2019-07-18 23:56:19 +0100

    replace geocoding API with locationIQ

--- src/util/here.js
+++ src/util/here.js
@@ -2,6 +2,7 @@ import {Keys} from './keys';
 
 const appId = Keys.appId;
 const appCode = Keys.appCode;
+const locationiqKey =Keys.locationiqKey;
 const pixabayKey = Keys.pixabayKey;
 let geo;
 
@@ -9,7 +10,7 @@ export const here = {
   /* takes a location string & queries here api to return a dict with the coordinates */
   geocode(location) {
     let encodedLocation = encodeURI(location);
-    const geocodeURL = `https://geocoder.cit.api.here.com/6.2/geocode.json?searchtext=${encodedLocation}&app_id=${appId}&app_code=${appCode}&gen=8`;
+    const geocodeURL = `https://us1.locationiq.com/v1/search.php?key=${locationiqKey}&q=${encodedLocation}&format=json`;
 
     return fetch(geocodeURL).then(response => {return response.json()}).then(responseJson => {
       return responseJson.Response.View[0].Result[0].Location.DisplayPosition;
@@ -20,27 +21,30 @@ export const here = {
   search(term, location) {
     let encodedTerm = encodeURI(term);
     let encodedLocation = encodeURI(location);
-    const geocodeURL = `https://geocoder.cit.api.here.com/6.2/geocode.json?searchtext=${encodedLocation}&app_id=${appId}&app_code=${appCode}&gen=8`;
+    const geocodeURL = `https://us1.locationiq.com/v1/search.php?key=${locationiqKey}&q=${encodedLocation}&format=json`;
 
 
     /// fails due to cors issue.
     return fetch(geocodeURL).then(
       response => {return response.json()}).then(
-      responseJson => {
-        return responseJson.Response.View[0].Result[0].Location.DisplayPosition;}).then(
+      responseJson => {return responseJson[0]}).then(
           coordinates => {
-            return fetch(`https://places.cit.api.here.com/places/v1/discover/search?app_id=${appId}&app_code=${appCode}&at=${coordinates.Latitude+"%2C"+coordinates.Longitude}&q=${encodedTerm}&pretty`,
-              {headers: {'Accept-Language' :'en-US,en;q=0.9'}})}).then(
-              response => {
-                console.log(response);
-      return response.json()}).then( responseJson => {
-        if (responseJson.results.items) {
-          return responseJson.results.items.map( poi => ({id: poi.id,
-            name: poi.title,
-            address: poi.vicinity,
-            category: poi.category.title,
-            hours: poi.openingHours.text,
-            isOpen: poi.openingHours.isOpen
+
+            const url = `https://places.cit.api.here.com/places/v1/discover/search?app_id=${appId}&app_code=${appCode}&at=${coordinates.lat+"%2C"+coordinates.lon}&q=${encodedTerm}&pretty`;
+            const method = 'GET';
+            const data = {"url": url, "method":method};
+            return fetch("http://localhost:8000/cors/", {
+                method: 'post',
+                body: JSON.stringify(data)})}).then(response => {
+                return response.json()}).then( responseJson => {
+                  console.log(responseJson);
+                  if (responseJson.results.items) {
+                    return responseJson.results.items.map( poi => ({id: poi.id,
+                      name: poi.title,
+                      address: poi.vicinity,
+                      category: poi.category.title,
+                      hours: poi.openingHours.text,
+                      isOpen: poi.openingHours.isOpen
           }));
         }
       });
@@ -59,18 +63,18 @@ export const here = {
     });
   },
 
-  /* searchh here maps for a map image. returns <img > with jpeg when f=1 */
+  /* searchh here maps for a map image. returns <img > with jpeg when f=1 _ Broken No Key*/
   getMapImage(location) {
     let encodedLocation = encodeURI(location);
-    const geocodeURL = `https://geocoder.cit.api.here.com/6.2/geocode.json?searchtext=${encodedLocation}&app_id=${appId}&app_code=${appCode}&gen=8`;
-
+    const geocodeURL = `https://us1.locationiq.com/v1/search.php?key=${locationiqKey}&q=${encodedLocation}&format=json`;
+console.log(geocodeURL);
     //fails due to cors error
     return fetch(geocodeURL).then(
       response => {return response.json()}).then(
       responseJson => {
-        return responseJson.Response.View[0].Result[0].Location.DisplayPosition;}).then(
+        return responseJson[0];}).then(
       coordinates => {
-      geo = coordinates.Latitude + "%2C" + coordinates.Longitude;
+      geo = coordinates.lat + "%2C" + coordinates.lon;
 
       return fetch(`https://image.maps.api.here.com/mia/1.6/mapview?c=${geo}&z=14&w=300&h=300&f=1&app_id=${appId}&app_code=${appCode}`);
     });
