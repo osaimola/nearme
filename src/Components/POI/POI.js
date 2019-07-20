a7a8f388fd6e15f01768af2cd5bf294abbc6428f
Osa <39202977+osaimola@users.noreply.github.com>
2019-07-20 07:28:11 +0100

    update poi component
    set default props
    handle displaying POIs with no opening hour values

--- src/Components/POI/POI.js
+++ src/Components/POI/POI.js
@@ -2,8 +2,12 @@ import React from 'react';
 import './POI.css'
 
 export class POI extends React.Component {
+  // this handles displaying if the business is open/closed or we dont have that info
   isOpen(decider) {
     if (decider) {
+      if (decider === "Unknown") {
+        return <span className="unknown">Unknown</span>;
+      }
       return <span className="open">Open</span>;
     } else {
       return <span >Closed</span>
@@ -11,8 +15,6 @@ export class POI extends React.Component {
   }
 
   render() {
-    // {this.isOpen(this.props.isOpen)}
-    //{this.props.hours}
     return (
       <div>
         <a className="card">
@@ -21,8 +23,8 @@ export class POI extends React.Component {
             <h1>{this.props.name}</h1>
             {this.props.address}
             <br/>
-
-
+            {this.props.hours}
+            {this.isOpen(this.props.isOpen)}
           </article>
         </a>
       </div>
@@ -30,3 +32,9 @@ export class POI extends React.Component {
   }
 
 }
+
+// set default values for the POI Component props.
+POI.defaultProps = {
+  isOpen: "Unknown",
+  hours: "No business hours information available."
+};
