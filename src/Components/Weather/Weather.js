34333ea68912ffa2123a01afc9204eae7eeb2fb5
Osa <39202977+osaimola@users.noreply.github.com>
2020-02-12 13:29:26 -0700

    add weather component and styling

---
+++ src/Components/Weather/Weather.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import "./Weather.css";
+
+class Weather extends Component {
+  generateIcon(icon) {
+    //return `url(http://openweathermap.org/img/wn/${icon}.png)`;
+    return `http://openweathermap.org/img/wn/${icon}.png`;
+  }
+
+  generateForecast(forecast) {
+    return `Expect ${forecast.description} and temperatures of ${Math.round(
+      forecast.temperature - 273
+    )}°C with a ${Math.round(forecast.feel - 273)}°C feel`;
+  }
+
+  render() {
+    return (
+      <div className="featured-item">
+        <div className="weathercard">
+          <div className="icon">
+            <img
+              src={this.generateIcon(this.props.weatherForecast.icon)}
+              className="iconimg"
+              alt=""
+            />
+          </div>
+
+          <h1>{this.generateForecast(this.props.weatherForecast)}</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Weather;
