a6522c38a161e72ff6959147d1d250da5ff47d22
Osa <39202977+osaimola@users.noreply.github.com>
2020-02-12 13:01:17 -0700

    switch to the places api and add weather forecast

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
-import React from 'react';
-import './App.css';
-import {SearchResults} from "./Components/SearchResults/SearchResults";
-import {SearchBar} from "./Components/SearchBar/SearchBar";
-import {here} from "./util/here";
+import React from "react";
+import "./App.css";
+import { SearchResults } from "./Components/SearchResults/SearchResults";
+import { SearchBar } from "./Components/SearchBar/SearchBar";
+import { here } from "./util/here";
+import { places } from "./util/places";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       mapImage: "",
-      poiList: []
+      poiList: [],
+      weather: {}
     };
     this.search = this.search.bind(this);
   }
 
   search(term, location) {
     //here.mapImageSearch( location).then( image => {console.log(image)});
-    here.search(term, location).then(pois =>
-     //console.log(pois));
-    this.setState({poiList: pois}));
+    //here.search(term, location).then(pois =>
+    //console.log(pois));
+    //this.setState({poiList: pois}));
     // get list of pois and get a small map image
+
+    places
+      .search(term, location)
+      .then(pois => this.setState({ poiList: pois }));
+
+    places
+      .getForcast(location)
+      .then(weather => this.setState({ weather: weather }));
   }
   render() {
     return (
       <div>
-        <SearchBar onSearch={this.search}/>
-        <SearchResults searchResults={this.state.poiList}/>
+        <SearchBar onSearch={this.search} />
+        <SearchResults
+          searchResults={this.state.poiList}
+          weatherForecast={this.state.weather}
+        />
       </div>
-  );
+    );
   }
 }
 
