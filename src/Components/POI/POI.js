f170232c3d3d41ef9822c6a432d9d78d358d2be2
Osa <39202977+osaimola@users.noreply.github.com>
2020-02-12 13:04:40 -0700

    updated to use data from places api

--- src/Components/POI/POI.js
+++ src/Components/POI/POI.js
@@ -1,36 +1,48 @@
-import React from 'react';
-import './POI.css'
+import React from "react";
+import "./POI.css";
 
 export class POI extends React.Component {
   // this handles displaying if the business is open/closed or we dont have that info
   isOpen(decider) {
     if (decider) {
-      if (decider === "Unknown") {
+      if (decider === "unknown") {
         return <span className="unknown">Unknown</span>;
       }
       return <span className="open">Open</span>;
     } else {
-      return <span >Closed</span>
+      return <span>Closed</span>;
     }
   }
 
   render() {
+    let poi = this.props.result;
+    let hoursChecker = poi.opening_hours
+      ? poi.opening_hours.open_now
+      : "unknown";
+    let photoId = poi.photos ? poi.photos[0].photo_reference : false;
+    let photoURL = photoId
+      ? `url(https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoId}&key=AIzaSyDoF9cMmMxTRvRVS61KE1GXTIaIqY6-M68)`
+      : "";
+
     return (
       <div>
         <a className="card">
-          <div className="thumb" style={{backgroundImage: "url(https://media-cdn.tripadvisor.com/media/photo-s/04/7a/26/22/west-edmonton-mall.jpg)"}}></div>
+          <div
+            className="thumb"
+            style={{
+              backgroundImage: photoURL
+            }}
+          ></div>
           <article>
-            <h1>{this.props.name}</h1>
-            {this.props.address}
-            <br/>
-            {this.props.hours}
-            {this.isOpen(this.props.isOpen)}
+            <h1>{poi.name}</h1>
+            {poi.formatted_address}
+            <br />
+            {this.isOpen(hoursChecker)}
           </article>
         </a>
       </div>
     );
   }
-
 }
 
 // set default values for the POI Component props.
