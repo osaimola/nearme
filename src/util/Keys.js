5f55c2e8c4b478c92590a940eb8bbabe57392eb0
Osa <osaimola@gmail.com>
2020-02-19 12:14:01 -0700

    add keys.js file

---
+++ src/util/Keys.js
@@ -0,0 +1,8 @@
+const Keys = {
+  apiKey: process.env.GOOGLEPLACESAPIKEY,
+  weatherAPIKey: process.env.OPENWEATHERAPIKEY,
+  appId: process.env.APPID,
+  appCode: process.env.APPCODE,
+  locationiqKey: process.env.LOCATIONIQKEY,
+  pixabayKey: process.env.PIXABAYKEY
+};
