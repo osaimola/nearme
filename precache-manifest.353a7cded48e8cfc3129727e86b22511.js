709e6b3a1bf49030fe920924639d28d7be4fae82
Osa <osaimola@gmail.com>
2020-02-20 11:21:44 -0700

    Updates

---
+++ precache-manifest.353a7cded48e8cfc3129727e86b22511.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6cfc292a9dc56d16588477c2fcc4f6",
+    "url": "/nearme/index.html"
+  },
+  {
+    "revision": "f37d4d44689cab1909e0",
+    "url": "/nearme/static/css/main.d44830e0.chunk.css"
+  },
+  {
+    "revision": "d04060b53753fffb2851",
+    "url": "/nearme/static/js/2.972ae6aa.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/nearme/static/js/2.972ae6aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f37d4d44689cab1909e0",
+    "url": "/nearme/static/js/main.551cd98e.chunk.js"
+  },
+  {
+    "revision": "1affe4ae3717ae585269",
+    "url": "/nearme/static/js/runtime-main.516a7a76.js"
+  },
+  {
+    "revision": "e89683bd79cb27e8b176941245540ffd",
+    "url": "/nearme/static/media/desktop.e89683bd.jpg"
+  },
+  {
+    "revision": "9c84c045126165e40e45e2bdf63da885",
+    "url": "/nearme/static/media/mobile.9c84c045.jpg"
+  },
+  {
+    "revision": "8d370db261577e593863a118f6600cbb",
+    "url": "/nearme/static/media/tablet.8d370db2.jpg"
+  }
+]);
